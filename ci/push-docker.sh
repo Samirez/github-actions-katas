363a5ae15a5d8a1a0dcba6411d6d5e76a0d1f9f3
Ahmadullah <ahnai17@student.sdu.dk>
2023-08-16 11:19:46 +0000

    docker uppercase username

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker_username_lower=$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')
-echo "$docker_password" | docker login ghcr.io --username "$docker_username_lower" --password-stdin
-docker push "ghcr.io/$docker_username_lower/micronaut-app:1.0-${GIT_COMMIT::8}"
-docker push "ghcr.io/$docker_username_lower/micronaut-app:latest" &
+echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
+docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/micronaut-app:latest" &
 wait
